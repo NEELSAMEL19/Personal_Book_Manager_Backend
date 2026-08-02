d0333e6447533d964705331accc16893dd1f798f
NEELSAMELL369 <neelsamel369@gmail.com>
2026-08-02 15:17:47 +0530

    rename root api response

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (_req, res) => {
-  res.send("Expense Tracker API is running...");
+  res.send("Personal Book API Expense Tracker API is running...");
 });
 
 app.use("/api/auth", authRoutes);
